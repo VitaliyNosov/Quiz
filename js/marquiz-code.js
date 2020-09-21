ed02c3aee057df634c523c1ed19a802f2d61cdd5
VitaliyNosov <webdeveloperstudio2017@gmail.com>
2020-09-21 14:39:26 +0300

    first commit

---
+++ js/marquiz-code.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    Marquiz.init({ 
+      host: '//quiz.marquiz.ru', 
+      id: '5eedf6c31cd2c20044b38b3b', 
+      autoOpen: 20, 
+      autoOpenFreq: 'once', 
+      openOnExit: false 
+    });
+  });
